5ceb7771d68c3b6900d295e3d1d81805e222c3b0
carlabraga1 <cb83224@gmail.com>
2025-05-19 18:48:11 -0300

    Finalizando Front end

--- frontend/src/pages/Products.vue
+++ frontend/src/pages/Products.vue
@@ -126,12 +126,10 @@ const fetchProdutos = async () => {
 };
 
 // Função para formatar a data para dd/mm/yyyy
+
 const formatDate = (dateStr: string): string => {
   if (!dateStr) return "";
-  const date = new Date(dateStr);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
+  const [year, month, day] = dateStr.substring(0, 10).split("-");
   return `${day}/${month}/${year}`;
 };
 
